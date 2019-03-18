5f070cb7ea283a2775d829eaa204353ee8462ba4
GordeySpb <gordey_89@mail.ru>
2019-03-18 22:47:51 +0300

    add hooks variant

---
+++ src/Filters/Filters.hooks.js
@@ -0,0 +1,161 @@
+import { useReducer } from 'react';
+
+import { TYPE, CONTAINING_OPERATION, EQUAL_OPERATION } from '../constants';
+
+const DEFAULT_TEXT_OPERATION = CONTAINING_OPERATION;
+const DEFAULT_NUMBER_OPERATION = EQUAL_OPERATION;
+
+const ADD_FILTER = 'ADD_FILTER';
+const UPDATE_VALUE = 'UPDATE_VALUE';
+const UPDATE_TYPE = 'UPDATE_TYPE';
+const UPDATE_OPERATION = 'UPDATE_OPERATION';
+const DELETE_FILTER = 'DELETE_FILTER';
+const GET_DATA = 'GET_DATA';
+const CLEAR_FILTERS = 'CLEAR_FILTERS';
+
+const getNewFilter = () => ({
+  type: TYPE.TEXT,
+  operation: DEFAULT_TEXT_OPERATION,
+  value: '',
+  id: +Date.now(),
+});
+
+function filterReducer(state, action) {
+  switch (action.type) {
+    case ADD_FILTER: {
+      return [...state, getNewFilter()];
+    }
+
+    case UPDATE_VALUE: {
+      return state.map((filter) => {
+        if (filter.id === +action.payload.id) {
+          return {
+            ...filter,
+            value: action.payload.value,
+          };
+        }
+
+        return filter;
+      });
+    }
+
+    case UPDATE_TYPE: {
+      return state.map((filter) => {
+        if (filter.id === +action.payload.id) {
+          return {
+            ...filter,
+            type: action.payload.type,
+            value: '',
+            operation:
+              action.payload.type === TYPE.TEXT
+                ? DEFAULT_TEXT_OPERATION
+                : DEFAULT_NUMBER_OPERATION,
+          };
+        }
+
+        return filter;
+      });
+    }
+
+    case UPDATE_OPERATION: {
+      return state.map((filter) => {
+        if (filter.id === +action.payload.id) {
+          return {
+            ...filter,
+            operation: action.payload.operation,
+          };
+        }
+
+        return filter;
+      });
+    }
+
+    case DELETE_FILTER: {
+      return state.filter((filter) => filter.id !== +action.payload.id);
+    }
+
+    case GET_DATA: {
+      const filters = state.reduce(
+        (res, cur) => {
+          const { type, id, ...restData } = cur;
+
+          if (type === TYPE.TEXT) {
+            res.text.push(restData);
+          }
+
+          if (type === TYPE.NUMBER) {
+            res.number.push(restData);
+          }
+
+          return res;
+        },
+        { number: [], text: [] }
+      );
+
+      console.log(filters);
+    }
+
+    case CLEAR_FILTERS: {
+      return [getNewFilter()];
+    }
+
+    default:
+      return state;
+  }
+}
+
+export default function useFilters() {
+  const [filters, dispatch] = useReducer(filterReducer, [getNewFilter()]);
+
+  const addNewFilter = () => dispatch({ type: ADD_FILTER });
+  const updateValue = (value, id) => {
+    dispatch({
+      type: UPDATE_VALUE,
+      payload: { value, id },
+    });
+  };
+
+  const updateType = (type, id) => {
+    dispatch({
+      type: UPDATE_TYPE,
+      payload: { type, id },
+    });
+  };
+
+  const updateOperation = (type, id) => {
+    dispatch({
+      type: UPDATE_OPERATION,
+      payload: { type, id },
+    });
+  };
+
+  const deleteFilter = (id) => {
+    dispatch({
+      type: DELETE_FILTER,
+      payload: { id },
+    });
+  };
+
+  const getData = () => {
+    dispatch({
+      type: GET_DATA,
+    });
+  };
+
+  const clearFilters = () => {
+    dispatch({
+      type: CLEAR_FILTERS,
+    });
+  };
+
+  return {
+    addNewFilter,
+    filters,
+    updateValue,
+    updateType,
+    updateOperation,
+    deleteFilter,
+    getData,
+    clearFilters,
+  };
+}
--- src/Filters/Filters.js
+++ src/Filters/Filters.js
@@ -1,177 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Filter from './Filter';
 import Controls from '../Controls';
-import { EQUAL_OPERATION, CONTAINING_OPERATION, TYPE } from '../constants';
 
-class Filters extends Component {
-  static DEFAULT_NUMBER_OPERATION = EQUAL_OPERATION;
+import useFilters from './Filters.hooks';
 
-  static DEFAULT_TEXT_OPERATION = CONTAINING_OPERATION;
+const Filters = () => {
+  const {
+    filters,
+    addNewFilter,
+    updateValue,
+    updateType,
+    updateOperation,
+    deleteFilter,
+    getData,
+    clearFilters,
+  } = useFilters();
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      filters: [
-        {
-          type: TYPE.TEXT,
-          operation: Filters.DEFAULT_TEXT_OPERATION,
-          value: '',
-          id: +Date.now(),
-        },
-      ],
-    };
-  }
-
-  setType = (type, id) => {
-    const { filters } = this.state;
-
-    const newFilters = filters.map((item) => {
-      if (item.id === +id) {
-        return {
-          ...item,
-          type,
-          value: '',
-          operation:
-            type === TYPE.TEXT
-              ? Filters.DEFAULT_TEXT_OPERATION
-              : Filters.DEFAULT_NUMBER_OPERATION,
-        };
-      }
-
-      return item;
-    });
-
-    this.setState({ filters: newFilters });
-  };
-
-  setValue = (value, id) => {
-    const { filters } = this.state;
-    const newFilters = filters.map((item) => {
-      if (item.id === +id) {
-        return {
-          ...item,
-          value,
-        };
-      }
-
-      return item;
-    });
-
-    this.setState({ filters: newFilters });
-  };
-
-  setOperation = (operation, id) => {
-    const { filters } = this.state;
-    const newFilters = filters.map((item) => {
-      if (item.id === +id) {
-        return {
-          ...item,
-          operation,
-        };
-      }
-
-      return item;
-    });
-
-    this.setState({ filters: newFilters });
-  };
-
-  getNewFilter = () => {
-    const newFilter = {
-      type: TYPE.TEXT,
-      operation: Filters.DEFAULT_TEXT_OPERATION,
-      value: '',
-      id: +Date.now(),
-    };
-    return newFilter;
-  };
-
-  addFilter = () => {
-    const { filters } = this.state;
-    const newFilter = {
-      type: TYPE.TEXT,
-      operation: Filters.DEFAULT_TEXT_OPERATION,
-      value: '',
-      id: +Date.now(),
-    };
-
-    const newFilters = [...filters, newFilter];
-
-    this.setState({ filters: newFilters });
-  };
-
-  clearFilters = () => {
-    this.setState({
-      filters: [this.getNewFilter()],
-    });
-  };
-
-  getData = () => {
-    const { filters } = this.state;
-
-    const newFilters = filters.reduce(
-      (res, cur) => {
-        const { type, id, ...restData } = cur;
-
-        if (type === TYPE.TEXT) {
-          res.text.push(restData);
-        }
-
-        if (type === TYPE.NUMBER) {
-          res.number.push(restData);
-        }
-
-        return res;
-      },
-      { number: [], text: [] }
-    );
-
-    console.log(newFilters);
-  };
-
-  deleteFilter = (id) => {
-    const { filters } = this.state;
-    const newFilterts = filters.filter((item) => item.id !== +id);
-
-    this.setState({ filters: newFilterts });
-  };
-
-  render() {
-    const { filters } = this.state;
-    return (
-      <>
-        <div className="list">
-          {filters.map(({ id, type, operation, value: newValue }) => (
-            <Filter
-              id={id}
-              key={id}
-              type={type}
-              operation={operation}
-              value={newValue}
-              count={filters.length}
-              onTypeChange={({ target: { value } }) => {
-                this.setType(value, id);
-              }}
-              onValueChange={({ target: { value } }) => {
-                this.setValue(value, id);
-              }}
-              onOperationChange={({ target: { value } }) => {
-                this.setOperation(value, id);
-              }}
-              onDeleteFilter={() => this.deleteFilter(id)}
-            />
-          ))}
-        </div>
-        <Controls
-          addFilter={this.addFilter}
-          getData={this.getData}
-          clearFilters={this.clearFilters}
-          count={filters.length}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className="list">
+        {filters.map(({ id, type, operation, value: newValue }) => (
+          <Filter
+            id={id}
+            key={id}
+            type={type}
+            operation={operation}
+            value={newValue}
+            count={filters.length}
+            onTypeChange={({ target: { value } }) => {
+              updateType(value, id);
+            }}
+            onValueChange={({ target: { value } }) => {
+              updateValue(value, id);
+            }}
+            onOperationChange={({ target: { value } }) => {
+              updateOperation(value, id);
+            }}
+            onDeleteFilter={() => deleteFilter(id)}
+          />
+        ))}
+      </div>
+      <Controls
+        addFilter={addNewFilter}
+        getData={getData}
+        clearFilters={clearFilters}
+      />
+    </>
+  );
+};
 
 export default Filters;
